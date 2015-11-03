e214ef7564531df14dd1bf9130603cec9252d9a9
Seshu1729 <seshu1729@gmail.com>
2015-11-03 09:05:30 +0530

    Submit Commit

--- src/isOlder.cpp
+++ src/isOlder.cpp
@@ -15,19 +15,19 @@ NOTES: 		Don't use any built-in C functions for comparisions. You are free to wr
 
 int isLeap(int year)
 {
-	int leap=1;
+	int leap = 1;
 	if(year%4!=0)
-		leap=0;
+		leap = 0;
 	else if(year%400==0)
-		leap=1;
+		leap = 1;
 	else if(year%100==0)
-		leap=0;
+		leap = 0;
 	return leap;
 }
 
-int isValid(int *date)
+int semanticValidation(int *date)
 {
-	int days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+	int days[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	if(isLeap(date[2]))
 		days[1]++;
 	if(days[date[1]-1]<date[0]||date[0]<1)
@@ -37,30 +37,40 @@ int isValid(int *date)
 	return 1;
 }
 
-void dateToNumber(char *dob,int *date)
+int syntaxValidation(char *dob, int *date)
 {
 	int index = 0;
+	if(dob[2]!='-'||dob[5]!='-'||dob[10]!='\0')
+		return 0;
 	while(*dob)
 	{
 		while(*dob!='-'&&*dob)
-			date[index] = (date[index]*10)+(*dob++)-48;
+		{
+			if(*dob<'0'||*dob>'9')
+				return 0;
+			date[index] = (date[index] * 10) + (*dob++) - 48;
+		}
 		index++;
+		if(*dob=='-')
+			dob++;
 	}
+	return 1;
 }
 
-int isOlder(char *dob1, char *dob2) 
+int isOlder(char *dob1, char *dob2)
 {
-	int date1[3] = {0,0,0},date2[3] = {0,0,0},index = 2;
-	dateToNumber(dob1,date1);
-	dateToNumber(dob2,date2);
-	if(!(isValid(date1)&&isValid(date2)))
+	int date1[3] = { 0, 0, 0 }, date2[3] = { 0, 0, 0 }, index = 2;
+	if(!(syntaxValidation(dob1, date1) && syntaxValidation(dob2, date2)))
+		return -1;
+	if(!(semanticValidation(date1) && semanticValidation(date2)))
 		return -1;
 	do
 	{
 		if(date1[index]>date2[index])
-			return 1;
-		else if(date1[index]<date2[index])
 			return 2;
-	}while(!(date1[index]-date2[index])&&index>=0);
+		else if(date1[index]<date2[index])
+			return 1;
+		index--;
+	}while(index>=0);
 	return 0;
 }
